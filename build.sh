d8ff41c66c1b985e3efb218b936c075450b0d8fd
Michael Rappold <mrappold@vwd.com>
2021-02-21 19:31:04 +0100

    ADD: initial commit

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+./7z.exe a -r exploration_extreme_modpack.zip exploration_extreme_modpack/
+./7z.exe a -r multiplayer_extreme_modpack.zip multiplayer_extreme_modpack/
+./7z.exe a -r singleplayer_extreme_modpack.zip singleplayer_extreme_modpack/
